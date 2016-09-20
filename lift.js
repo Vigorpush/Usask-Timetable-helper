ca0fd1f7b2b56685663f1af59a3405cc23b92c76
Vigor <Vigorzjw@gmail.com>
2016-09-20 14:29:50 -0600

    Trying adding date picker

--- lift.js
+++ lift.js
@@ -6,6 +6,7 @@
 //@author       Zang JiaWei, Nobleman Chukwu, Bengin Lee, Mark Nguyen
 //@match        https://pawnss.usask.ca/ban/*
 //@require   	http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+//@require		http://www.eyecon.ro/datepicker/js/datepicker.js
 //@grant        none
 //==/UserScript==
 
@@ -16,13 +17,29 @@
 var newCSS = "";
 newCSS += "";
 var maincolor = "#2c8fe4";
-
+var newjs = "http://www.eyecon.ro/datepicker/js/datepicker.js";
+var search = document.querySelector('.pagebodydiv > form:nth-child(2) > table:nth-child(1)');
 
 $(document).ready(function() {
 	$(".ddlabel A").css({ 'color': '#39a3b1', 'font-size': '100%' });
 	CreateButton();
 	addStyle(newCSS);
+	addJs(newjs);//dd
 });
+
+
+function DatePick(){//dd
+	$('input[name=start_date_in]').DatePicker({
+		format:'m/d/Y',
+		date: $('#input[name=start_date_in]').val(),
+		current: $('#input[name=start_date_in]').val(),
+		starts: 1,
+		position: 'r',
+		onBeforeShow: function(){
+			$('#input[name=start_date_in]').DatePickerSetDate($('#input[name=start_date_in]').val(), true);
+		},
+	});
+}
 
 /**
  * Creating the share button 
@@ -47,6 +64,11 @@ function addStyle(css){
 	document.head.appendChild(document.createElement("style")).textContent = css;
 }
 
+
+function addJs(js){//dd
+	
+	document.head.appendChild(document.createElement("script")).textContent = js;
+}
 /**
  * 
  * Function Share function Caller
@@ -64,14 +86,18 @@ function ShareAction(){
 	lnk.href = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css";
 	document.head.appendChild(lnk);
 	document.querySelector(".pagebodydiv > div.infotextdiv").style.display = "none";
-	var search = document.querySelector('.pagebodydiv > form:nth-child(2) > table:nth-child(1)');
+	/*
+	 * var search = document.querySelector('.pagebodydiv > form:nth-child(2) > table:nth-child(1)');
 	if (search) {
 		console.log("Found the search thingy");
 	}
 	search.style.display = 'none';
+	
+	*/
 	var tableStyle = 'table table-striped table-bordered table-condensed table-responsive';
 	var timeTable = document.querySelector('.datadisplaytable');
 	timeTable.className = tableStyle;
+	$("input[name=start_date_in]").addEventListener("click", DatePick); //dd
 
 })();
 
