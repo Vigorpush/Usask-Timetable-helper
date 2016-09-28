163275df8347e618fc68f938177e1c999e1c8fb6
RedPenguin420 <otaku.tnguyen@gmail.com>
2016-09-28 17:31:32 -0600

    Wrote the rid_number() and and replace_title() functions.
    Needs more work.

--- lift.js
+++ lift.js
@@ -23,6 +23,8 @@ $(document).ready(function() {
 	$(".ddlabel A").css({ 'color': '#39a3b1', 'font-size': '100%' });
 	CreateButton();
 	addStyle(newCSS);
+	replace_title();
+	rid_number();
 });
 
 
@@ -38,7 +40,17 @@ function navigation_term(){
  * Replace the title with "Student schedule for 2016-2017 Term 1"
  * */
 function replace_title(){
+	//TODO: Somehow obtain the current term and year. Right now it's just hard-coded.
 	
+	//Changes the title, "Student Schedule by Day and Time", to "Student Schedule for 2016 - 2017 Term 1".
+	//In doing so, the information on the right, which includes the time, date and student's identity, is removed.
+	//If we want to keep that information, we'll need to create a table.
+	document.querySelector(".pagetitlediv").innerHTML = "Student Schedule for 2016 - 2017 Term 1<br><br>";
+	document.querySelector(".pagetitlediv").style.color = '#39a3b1';
+	document.querySelector(".pagetitlediv").style.fontSize = 'x-large';
+	
+	//Removes the useless information on the page that no one reads.
+	document.querySelector(".pagebodydiv > div.infotextdiv").style.display = 'none';
 }
 
 
@@ -62,7 +74,36 @@ function navigating_weeks(){
  * Get rid of the hideous number that shows the number of weeks you have been attending the UofS
  * */
 function rid_number(){
+
+	//TODO: Fix the Previous and Next Week links to correctly display the current week number and date.
+	//TODO: Somehow obtain the current week number. Right now it's just hard-coded, not what we want.
 	
+	//Remove the useless information detailing the total number of weeks a student has been attending the U of S.
+	document.querySelector(".fieldmediumtext").style.display = 'none';
+	
+	var monthNames = [
+		"January", "February", "March", "April",
+		"May", "June", "July", "August",
+		"September", "October", "November", "December"
+	];
+	
+	var today = new Date();
+	var date = today.getDate();
+	var monthIndex = today.getMonth();
+	var year = today.getFullYear();
+	
+	var day = today.getDay();
+	for(var i = -1; day > 0; day--){
+		i++
+	}	
+	
+	var weekOf = date - i;
+	
+	var nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
+	
+	//Changes "Week of Sep 26, 2016" to "Week # September 26, 2016"
+	document.querySelector(".fieldlargetext").innerHTML = 
+		('Week 4 ' + monthNames[monthIndex] + ' ' + weekOf + ', ' + year);
 }
 
 function DatePick(){//dd
