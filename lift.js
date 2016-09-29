93daae1408431e2fdaa4e7fe6a95630a19e8af99
Vigor <Vigorzjw@gmail.com>
2016-09-28 18:53:03 -0600

    merged teammates code for differents fucntionalities

--- lift.js
+++ lift.js
@@ -5,9 +5,13 @@
 //@description  A bit of Javascript for enhancing the LAF of the timetable page on the usask website
 //@author       Zang JiaWei, Nobleman Chukwu, Bengin Lee, Mark Nguyen
 //@match        https://pawnss.usask.ca/ban/*
-//@require   	http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-//@require		http://www.eyecon.ro/datepicker/js/datepicker.js
-//@grant        none
+// @require		https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @require		https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js
+// @require		http://www.eyecon.ro/datepicker/js/datepicker.js
+
+// @resource	bootStrap https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
+// @grant		GM_getResourceText
+// @grant		GM_addStyle
 //==/UserScript==
 
 /**
@@ -20,15 +24,14 @@ var search = document.querySelector('.pagebodydiv > form:nth-child(2) > table:nt
 var script ="http://www.eyecon.ro/datepicker/js/datepicker.js";
 
 $(document).ready(function() {
+	addStyleSheet('bootStrap');
 	$(".ddlabel A").css({ 'color': '#39a3b1', 'font-size': '100%' });
 	CreateButton();
 	addStyle(newCSS);
 	replace_title();
 	rid_number();
+	highlightCurrentDay();
 });
-
-
-
 /**
  * Add button above the schedule to allow navigation between terms
  * */
@@ -47,8 +50,7 @@ function replace_title(){
 	//If we want to keep that information, we'll need to create a table.
 	document.querySelector(".pagetitlediv").innerHTML = "Student Schedule for 2016 - 2017 Term 1<br><br>";
 	document.querySelector(".pagetitlediv").style.color = '#39a3b1';
-	document.querySelector(".pagetitlediv").style.fontSize = 'x-large';
-	
+	document.querySelector(".pagetitlediv").style.fontSize = 'x-large';
 	//Removes the useless information on the page that no one reads.
 	document.querySelector(".pagebodydiv > div.infotextdiv").style.display = 'none';
 }
@@ -60,8 +62,6 @@ function replace_title(){
 function hightlight_current(){
 	
 }
-
-
 /**
  * Large buttons for navigating the weeks. Kinda like the Google images page
  * */
@@ -69,12 +69,10 @@ function navigating_weeks(){
 	
 }
 
-
 /**
  * Get rid of the hideous number that shows the number of weeks you have been attending the UofS
  * */
 function rid_number(){
-
 	//TODO: Fix the Previous and Next Week links to correctly display the current week number and date.
 	//TODO: Somehow obtain the current week number. Right now it's just hard-coded, not what we want.
 	
@@ -137,6 +135,82 @@ function CreateButton(){
 
 /**
  * adding style into head
+ */
+function addStyleSheet(resName) {
+    var style = GM_getResourceText(resName);
+    GM_addStyle(style);
+}
+
+
+/**
+
+
+
+
+
+ * Function to highlight all the cells that correspond to the current
+ * day
+ */
+function highlightCurrentDay() {
+    var d = new Date().getDay() - 1;
+    var timeTable = $('table.datadisplaytable')
+        .addClass("table table-striped table-bordered table-responsive table-condensed");
+
+
+
+
+
+
+
+    // Since the table cells are staggered, we need a way of
+    // knowing where each cell begins and ends
+function Cell(occuppiedBefore, row) {
+        this.ocb = occuppiedBefore;
+        this.height = row;
+    }
+
+    var rowInfo = [new Cell(0, 0),
+        new Cell(0, 0),
+        new Cell(0, 0),
+        new Cell(0, 0),
+        new Cell(0, 0),
+        new Cell(0, 0),
+        new Cell(0, 0)
+    ];
+
+
+
+
+
+    $(timeTable).find('tr').slice(1)
+        .each(function (_) {
+            var currRow = $(this);
+            $.each(rowInfo, function (key, cell) {
+                // height of zero means that the original row ends here and a new row starts
+                if (cell.height == 0) {
+                    var currCell = currRow.find('td:nth-of-type(' + (key - cell.ocb + 1) + ')');
+                    var span = $(currCell).attr('rowspan');
+                    cell.height = span ? parseInt(span) : 1;
+
+
+
+                    if (key == d) {
+                        $(currCell).css({
+                            'background-color': '#2daf6e'
+                        }).find('a').css({
+                            'color': 'white'
+                        });
+                    }
+                }
+                if (key > 0) {
+                    cell.ocb = rowInfo[key - 1].ocb + (rowInfo[key - 1].height > 0);
+                }
+                cell.height--;
+            });
+        });
+}
+/**
+ * adding style into head
  * */
 function addStyle(css){
 	document.head.appendChild(document.createElement("style")).textContent = css;
@@ -177,6 +251,3 @@ function ShareAction(){
 	timeTable.className = tableStyle;
      $( "input[name='goto_date_in']" ).on( "click", DatePick );
 })();
-
-
-
