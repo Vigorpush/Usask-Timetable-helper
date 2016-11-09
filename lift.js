a7db4e2a956eca30e5d4b2764da93dbd78a691d5
zvros <benginlee@hotmail.com>
2016-11-09 04:31:52 +0000

    time is in PM AM now

--- lift.js
+++ lift.js
@@ -316,7 +316,15 @@ function makeTimeLabel() {
 }
 
 function javascriptSux() {
-    time.innerHTML = new Date();    
+    var date = new Date();
+    var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    var am_pm = date.getHours() >= 12 ? "PM" : "AM";
+    hours = hours < 10 ? "0" + hours : hours;
+    var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    fucko = hours + ":" + minutes + ":" + seconds + " " + am_pm;
+    time.innerHTML = fucko;    
+    // time.innerHTML = new Date();    
 }
 
 /*
